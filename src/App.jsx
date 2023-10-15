cba598dd0952c75612ca5e7b7da3c938a1d39e1c
Tu Dinh <dinh2644.mail@gmail.com>
2023-10-15 02:11:05 -0400

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Leaderboard from "./components/Leaderboard";
+import Header from "./components/Header";
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 const App = () => {
-  return <div>App</div>;
+  const [leaderboardData, setLeaderboardData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      let response = await axios.get("https://api.chess.com/pub/leaderboards/");
+      const liveBlitzData = response.data["live_blitz"];
+      setLeaderboardData(liveBlitzData);
+    };
+    fetchData();
+    console.log(leaderboardData);
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Leaderboard leaderboardData={leaderboardData} />
+      <footer className="footer mt-5 mb-5">&#169;Chess.com</footer>
+    </>
+  );
 };
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <h1 className="chessHeader mb-5">
+        Live<span>Blitz</span>Rating
+      </h1>
+    </>
+  );
+};
+
+export default Header;
